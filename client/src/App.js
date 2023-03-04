feedb7e3dc593548ad2e9677254c8dfd27200f6f
KhamzatKot <1000000000020z@gmail.com>
2023-03-04 15:09:00 +0300

    Старт нвого проекта и создания фронта и бека

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div><p>dlkjbnadupfibnd'flibndsliubhsdf'bns;etiughadfi'bnseiuaghsiofbnewtiubaefobnadubigebpaoid</p></div>
+    </div>
+  );
+}
+
+export default App;
